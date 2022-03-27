35a8a99597df4bf89b15edf9493bb9336102db41
Toan Nguyen <toan.nguyen@hitatek.io>
2022-03-27 10:51:28 +0700

    config for react-ts

---
+++ react-ts/jest.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const { jsWithTs: tsjPreset } = require('ts-jest/presets');
+const ignorePatterns = ['.d.ts$'];
+
+module.exports = {
+    preset: 'ts-jest/presets/js-with-ts',
+    globals: {
+        'ts-jest': {
+            tsConfig: {
+                target: 'es2015',
+            }
+        }
+    },
+    rootDir: __dirname,
+    roots: ['<rootDir>/src'],
+    transform: {
+        ...tsjPreset.transform,
+        '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+        '^.+.css$': require.resolve('react-scripts/config/jest/cssTransform.js'),
+        '^(?!.*.(ts|tsx|js|jsx|css|json)$)': require.resolve('react-scripts/config/jest/cssTransform.js'),
+    },
+    transformIgnorePatterns: [
+        'node_modules',
+        '^.+module.(css|sass|scss)$'
+    ],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+    ],
+    coveragePathIgnorePatterns: [
+        ...ignorePatterns,
+        'src/(.*)/(.*).d.ts',
+        'src/index.ts',
+        'node_modules/(.*)'
+    ],
+    setupFiles: [require.resolve('react-app-polyfill/jsdom.js')],
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    testRegex: [
+        '((__tests__|test)/(?!__mocks__).*|(\\.|/)(test|spec))\\.(tsx|ts)?$',
+    ],
+    testEnvironment: 'jest-environment-jsdom-sixteen',
+    testPathIgnorePatterns: ignorePatterns,
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    snapshotSerializers: ['enzyme-to-json/serializer'],
+    modulePath: [path.join(__dirname, 'src')],
+    testResultsProcessor: 'jest-sonar-reporter',
+};
